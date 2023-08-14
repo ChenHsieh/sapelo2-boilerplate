7e3fd24feffc3d09e7b82645186e3907211d360a
Chen Hsieh <70214580+ChenHsieh@users.noreply.github.com>
2023-08-14 13:15:16 -0400

    Create 03run_for_sapelo2.sh

---
+++ apps/MCScanx/03run_for_sapelo2.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#SBATCH -J mcscanx_h1
+#SBATCH --partition batch
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --time=168:00:00
+#SBATCH --mem=64gb
+#SBATCH --mail-user=youremail@uga.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --output=mcscanx_h1.%j.out
+#SBATCH --error=mcscanx_h1.%j.err
+
+
+date
+
+cd $SLURM_SUBMIT_DIR
+
+ml MCScanX
+
+MCScanX ./h1
+
+duplicate_gene_classifier ./h1
